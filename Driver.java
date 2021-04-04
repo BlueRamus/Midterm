9eab307cdfce75206b3cf9d0a7d5fd9784f2683d
Moiz Wajahat <72225442+BlueRamus@users.noreply.github.com>
2021-04-05 00:01:47 +0500

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pd3_26.midterm_project;
+
+/**
+ *
+ * @author DELL
+ */
+public class Driver {
+    public static void main(String[] args)
+    {
+        ChooseAccountType ChAT = new ChooseAccountType();
+        ChAT.setVisible(true);
+        
+    }
+}
